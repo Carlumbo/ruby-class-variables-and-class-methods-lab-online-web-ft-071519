5ea34129c7364866473aaeed53c35e5f9ceec508
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-28 17:43:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
 class Song 
   attr_accessor :name, :artist , :genre
-  # attr_reader :name, :artist, :genre
-  def initial
+  # attr_reader :name, :artist, :genres 
+  @@count = 0 
+  def initialize
+    @@count += 1
   end 
   
   
