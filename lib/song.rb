2866dac78caa8561f45f8ae9b265468c7d106fae
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-28 17:45:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,18 @@
 class Song 
-
-  
+  def name=(name)
+    @name = name
+  end 
+  def name 
+    @name 
+  end 
+  def artist=(artist)
+    @artist = artist
+  end 
+  def genre=(genre)
+    @genre = genre 
+  end 
+  def genre 
+    @genre 
+  end 
   
 end 
